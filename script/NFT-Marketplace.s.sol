8d45e93b72ca8b27af6b6b3bb7e3d7698c246128
theatharvamuley10 <atharvablockchain@gmail.com>
2025-06-09 01:35:12 +0530

    feat: add scripts for deployment and interaction

---
+++ script/NFT-Marketplace.s.sol
@@ -0,0 +1,21 @@
+// scripts are never deployed
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import {Script, console} from "forge-std/Script.sol";
+import {NFTMarketplace} from "../src/NFT-Marketplace.sol";
+
+contract NFTMarketplaceScript is Script {
+    NFTMarketplace public nftMarketplace;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        nftMarketplace = new NFTMarketplace();
+
+        vm.stopBroadcast();
+    }
+}
