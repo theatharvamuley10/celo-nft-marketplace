32effed2a32700875a5725f7211e61b87a201919
theatharvamuley10 <atharvablockchain@gmail.com>
2025-06-16 11:41:28 +0530

    Updated deploy script

--- script/DeployNFTMarketplace.s.sol
+++ script/DeployNFTMarketplace.s.sol
@@ -1,7 +1,7 @@
 // scripts are never deployed
 
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.30;
+pragma solidity 0.8.30;
 
 import {Script, console} from "forge-std/Script.sol";
 import {NFTMarketplace} from "../src/NFT-Marketplace.sol";
@@ -9,13 +9,9 @@ import {NFTMarketplace} from "../src/NFT-Marketplace.sol";
 contract DeployNFTMarketplace is Script {
     NFTMarketplace public nftMarketplace;
 
-    function setUp() public {}
-
     function run() public returns (NFTMarketplace) {
         vm.startBroadcast();
-
         nftMarketplace = new NFTMarketplace();
-
         vm.stopBroadcast();
 
         return nftMarketplace;
